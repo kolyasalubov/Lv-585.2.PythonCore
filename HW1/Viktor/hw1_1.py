24fbc49fe1531f7e086d7ce5a32f25dcca2a7e11
ViktorPshenyshnyi <pshenyshnyi.viktor@gmail.com>
2021-03-26 20:45:54 +0200

    Added hw1

---
+++ HW1/Viktor/hw1_1.py
@@ -0,0 +1,4 @@
+user_name = input('What is your name? ')
+user_age = input('How old are you? ')
+user_city = input('Where do you live? ')
+print(f'Hello, {user_name}.\nYour age is {user_age}.\nYou live in {user_city}')
