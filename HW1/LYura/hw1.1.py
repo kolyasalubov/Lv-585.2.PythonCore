fc90a69a337965214698f0bce35ebbcd80a0c17f
Yura Leshchuk <yura.leschuk03@gmail.com>
2021-03-27 12:56:22 +0200

    added hw1

---
+++ HW1/LYura/hw1.1.py
@@ -0,0 +1,8 @@
+ask_name = input("What is your name?" " ")
+print("Hello", str(ask_name))
+
+ask_age = input("How old are you?" " ")
+print("Your age is", int(ask_age))
+
+ask_city = input("Where do you live?" " ")
+print("You live in", str(ask_city))
