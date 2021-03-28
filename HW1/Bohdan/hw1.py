bd665b051475b49bda03f9d426d07aa434649b9a
Bohdan <wolf6961@gmail.com>
2021-03-28 21:29:56 +0300

    added HW1, task_1, task_2

---
+++ HW1/Bohdan/hw1.py
@@ -0,0 +1,7 @@
+user_name = input('What is your name?\n>>>')
+user_age = input('How old are you?\n>>>')
+user_location = input('Where do you live?\n>>>')
+
+print('Hello', user_name.title())
+print('Your age is', user_age)
+print('Your live in', user_location.title())
---
+++ HW1/Bohdan/hw2.py
@@ -0,0 +1,8 @@
+a = float(input('Please, enter a number "a"\n>>>'))
+b = float(input('Please, enter a number "b"\n>>>'))
+
+print('a+b', a + b, sep='=')
+print('a-b', a - b, sep='=')
+print('a*b', a * b, sep='=')
+print('a/b', a / b, sep='=')
+print('a**b', a ** b, sep='=')
