e94dcd37a74d0b006c4a8ad2d0c079c3dd52fc4e
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-03-28 15:22:29 +0300

    Added HM 1.1

---
+++ HW1/lypovetsky/1.1_name.py
@@ -0,0 +1,17 @@
+# name
+print('What is your name?')
+name = input()
+print('Hello,', name)
+
+# age
+print('How old are you?')
+age = input()
+print('Your age is', age)
+
+# city
+print('Where do you live?')
+city = input()
+print('You live in', city)
+
+# all together
+print('...so,', name + '.', 'Your age is', age, 'and you live in', city)
