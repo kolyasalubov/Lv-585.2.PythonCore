1fa7e500960b65ea9fb39f2618a514a446704bec
Dmytro Melnychuk <melny4uk.d@gmail.com>
2021-03-28 14:37:07 +0300

    added hw1_1 and hw1_2

---
+++ HW1/Melnychuk/hw1_1.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+city = input("Where do you live? ")
+
+print(f"Hello, {name}")
+print(f"Your age is {age}")
+print(f"You live in {city}")
