0646324da698daa97d1cfde90ca61fbdd639752f
Vita Kotenko <mialkovska.viktoria@gmail.com>
2021-03-29 22:03:45 +0300

    added HW1

---
+++ HW1/1_1.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+print("Hello, ",name)
+age = input ("How old are you?: ")
+print("Your age is", int(age))
+city = input("Where do you live?: ")
+print("You live in ", city)
+
