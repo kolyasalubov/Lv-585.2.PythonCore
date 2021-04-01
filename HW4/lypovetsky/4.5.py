8cf9bacfb1c52cef27d1b3af46822f3df98d7d51
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-04-01 13:00:39 +0300

    Fixed float list

--- HW4/lypovetsky/4.5.py
+++
@@ -1,9 +0,0 @@
-# Task2. Create a list that contains elements of integer type, then use the loop to change the type of these elements to a floating type.
-
-# (Hint: use the built-in float() function).
-list = [1, 2, 3]
-i = len(list)
-while i >= 1:
-    float(list[i - 1])
-    print(float(list[i - 1]))
-    i -= 1
