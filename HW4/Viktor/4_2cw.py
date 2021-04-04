1366e8c40402e7590f943e00065be37cfda08253
ViktorPshenyshnyi <pshenyshnyi.viktor@gmail.com>
2021-04-04 15:11:42 +0300

    Added cw homework

---
+++ HW4/Viktor/4_2cw.py
@@ -0,0 +1,4 @@
+def reverse(st):
+    st = str(st).split()[::-1]
+    st =' '.join(st)
+    return st
---
+++ HW4/Viktor/4_3cw.py
@@ -0,0 +1,6 @@
+def greet(name):
+    if name != "Johnny":
+        return "Hello, {name}!".format(name=name)
+    else:
+        return "Hello, my love!"
+    
