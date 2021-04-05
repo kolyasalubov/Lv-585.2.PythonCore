f11873efb6e1859c3d403f34bf043efb35d64154
ViktorPshenyshnyi <pshenyshnyi.viktor@gmail.com>
2021-04-05 21:28:32 +0300

    Added HW6

---
+++ HW6/Viktor/6_1.py
@@ -0,0 +1,10 @@
+def largest_number(first_number, second_number):
+    """
+    Funcion largest_number
+    input parameters: first_number, second_number
+    type input parameters - int
+    return str
+    """
+    return "The largest number is "+str(max(first_number, second_number))
+
+print(largest_number(53, 83))
---
+++ HW6/Viktor/6_3.py
@@ -0,0 +1,10 @@
+def number_of_characters(st, el):
+    """
+    Funcion number_of_characters
+    input parameters: st, el
+    type input parameters - str
+    return int
+    """
+    return st.count(el)
+    
+print(number_of_characters("Funcion number_of_characters", "a"))
