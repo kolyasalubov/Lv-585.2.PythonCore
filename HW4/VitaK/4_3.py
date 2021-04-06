9e50345ca13eb502749f02f378c4dfeac48477bb
Vita Kotenko <mialkovska.viktoria@gmail.com>
2021-04-06 20:50:07 +0300

    changed HW4_3

--- HW4/VitaK/4_3.py
+++ HW4/VitaK/4_3.py
@@ -4,11 +4,14 @@ number2 = 1
 if enter_number == 0: 
     print (0)
 else :
-    print(number1, number2, end=' ')
-    for number in range (enter_number):
+    list1 = [number1, number2]
+    
+    for number in range (2,enter_number):
         number = number1 + number2
         number1 = number2
         number2= number
         if number > enter_number:
             break
-        print (number, end=' ')
+        list1.append(number)
+    print (list1)
+
