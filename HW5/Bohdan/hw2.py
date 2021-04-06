2ca277e1318e5af1b7571cccfedf3ede5d44d954
Bohdan <wolf6961@gmail.com>
2021-04-06 17:53:43 +0300

    HW5, added task2

---
+++ HW5/Bohdan/hw2.py
@@ -0,0 +1,7 @@
+user_name = "First"
+user_login = input("Enter your login:\n>>> ")
+
+while user_login != user_name :
+    user_login = input("Invalid login. Please try again\n>>>")
+
+print(f"Hello, {user_login.title()}")
