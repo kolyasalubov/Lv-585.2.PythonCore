9e6a7e8b42bd9d8aa8fdedd7f0a442f8250103e1
Dennis Lypovetsky <tsssssssss@gmail.com>
2021-04-10 18:12:43 +0300

    Added HW 6.6

---
+++ HW6/lypovetsky/6.6_string-length.py
@@ -0,0 +1,15 @@
+# Write a function that calculates the number of characters included in a given string
+def charNumOfAString(string="string"):
+    """
+    Returns calculated number of characters included in a given string.
+
+        Parametrs:
+            str (string): a given string to calcule
+
+        Returns:
+            len(list(str)): length of string converdet to a list
+    """
+    return len(list(str))
+
+
+# print(charNumOfAString())
