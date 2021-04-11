d20cd93929cbd531f12ba09216e72de10104d746
Bohdan <wolf6961@gmail.com>
2021-04-12 00:45:44 +0300

    HW7,  hw2 added __doc__

--- HW7/Bohdan/hw2.py
+++ HW7/Bohdan/hw2.py
@@ -2,6 +2,11 @@ import re
 
 
 def enter_password():
+    """
+    Entering user password and length check password
+    user_password = input str
+    :return: str
+    """
     global user_password
     user_password = input('Enter password.\n >>>')
     if len(user_password) < 6 or len(user_password) > 12:
@@ -11,6 +16,11 @@ def enter_password():
 
 
 def checking_big_letters(password):
+    """
+    checking uppercase letters
+    :param password: str from enter_password
+    :return: str or int
+    """
     template_look = r"[A-Z]+"
     result = re.findall(template_look, password)
     if len(result) == 0:
@@ -20,6 +30,11 @@ def checking_big_letters(password):
 
 
 def checking_little_letters(password):
+    """
+    checking lowercase  letters
+    :param password: str from enter_password
+    :return: str or int
+    """
     template_look = r"[a-z]+"
     result = re.findall(template_look, password)
     if len(result) == 0:
@@ -29,6 +44,11 @@ def checking_little_letters(password):
 
 
 def checking_digit(password):
+    """
+    check digits
+    :param password: str from enter_password
+    :return: str or int
+    """
     template_look = r"[0-9]+"
     result = re.findall(template_look, password)
     if len(result) == 0:
@@ -38,6 +58,11 @@ def checking_digit(password):
 
 
 def checking_symbol(password):
+    """
+    check $, #, @
+    :param password: str from enter_password
+    :return: str or int
+    """
     template_look = r"[$, #, @]+"
     result = re.findall(template_look, password)
     if len(result) == 0:
