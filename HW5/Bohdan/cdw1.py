f52f5b896a2cef03c623f49afbd5b920faa54646
Bohdan <wolf6961@gmail.com>
2021-04-12 11:52:50 +0300

    HW5,  cdw1 update

--- HW5/Bohdan/cdw1.py
+++ HW5/Bohdan/cdw1.py
@@ -2,5 +2,6 @@ def zero_fuel(distance_to_pump, mpg, fuel_left):
     if float(distance_to_pump) > 0:
         if (distance_to_pump / mpg) <= fuel_left:
             return True
-        else: return False
+        else:
+            return False
     return False
