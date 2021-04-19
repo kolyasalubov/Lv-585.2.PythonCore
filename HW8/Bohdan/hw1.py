a414e8061ecb606dd80c8e9c19faf582a11e2300
Bohdan <wolf6961@gmail.com>
2021-04-19 21:12:14 +0300

    HW8, update hw1

--- HW8/Bohdan/hw1.py
+++ HW8/Bohdan/hw1.py
@@ -1,18 +1,37 @@
-
 class Polygon:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def init(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = [0 for i in range(no_of_sides)]
 
-    def square_polygon(self):
-        s = self.a * self.b
-        return s
+    def inputSides(self):
+        self.sides = [float(input(f"Enter side {str(i + 1)}: "))
+                      for i in range(self.n)]
+
+    def dispSides(self):
+        for i in range(self.n):
+            print(f"Side {i + 1} is {self.sides[i]}")
+
+
+class Triangle(Polygon):
+    def init(self):
+        # Polygon.init(self, 3)
+        super().init(3)
+
+    def findArea(self):
+        a, b, c = self.sides
+        # calculate the semi-perimeter
+        p = (a + b + c) / 2
+        area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+        print(f"The area of the triangle is {area}")
 
 
 class Rectangle(Polygon):
-    pass
+    def init(self):
+        # Polygon.init(self, 3)
+        super().init(2)
 
-
-m1 = Rectangle(20, 20).square_polygon()
-
-print(m1)
+    def findArea(self):
+        a, b = self.sides
+        # calculate the semi-perimeter
+        area = a * b
+        print(f"The area of the Rectangle is {area}")
